37819c02aed4ef796479106760d1949cd9c62b02
dbxsuper54 <103365299+dbxsuper54@users.noreply.github.com>
2022-04-27 08:49:32 +0200

    Add files via upload

---
+++ ZSG/ZSG/Bullet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace ZSG
+{
+    class Bullet
+    {
+        public string direction;
+        public int bulletLeft;
+        public int bulletTop;
+
+        private int speed = 20;
+        private PictureBox bullet = new PictureBox();
+        private Timer bulletTimer = new Timer();
+
+        public void MakeBullet(Form form)
+        {
+
+            bullet.BackColor = Color.White;
+            bullet.Size = new Size(5, 5);
+            bullet.Tag = "bullet";
+            bullet.Left = bulletLeft;
+            bullet.
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ZSG/ZSG/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ZSG
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtAmmo = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.health = new System.Windows.Forms.Label();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAmmo
+            // 
+            this.txtAmmo.AutoSize = true;
+            this.txtAmmo.Font = new System.Drawing.Font("Viner Hand ITC", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtAmmo.ForeColor = System.Drawing.Color.Maroon;
+            this.txtAmmo.Location = new System.Drawing.Point(12, 9);
+            this.txtAmmo.Name = "txtAmmo";
+            this.txtAmmo.Size = new System.Drawing.Size(114, 37);
+            this.txtAmmo.TabIndex = 0;
+            this.txtAmmo.Text = "Ammo: 0";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Viner Hand ITC", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtScore.ForeColor = System.Drawing.Color.Maroon;
+            this.txtScore.Location = new System.Drawing.Point(316, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(101, 37);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Kills: 0";
+            // 
+            // health
+            // 
+            this.health.AutoSize = true;
+            this.health.Font = new System.Drawing.Font("Viner Hand ITC", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.health.ForeColor = System.Drawing.Color.Maroon;
+            this.health.Location = new System.Drawing.Point(603, 9);
+            this.health.Name = "health";
+            this.health.Size = new System.Drawing.Size(109, 37);
+            this.health.TabIndex = 2;
+            this.health.Text = "Health:";
+            // 
+            // healthBar
+            // 
+            this.healthBar.Location = new System.Drawing.Point(708, 12);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(202, 29);
+            this.healthBar.TabIndex = 3;
+            this.healthBar.Value = 100;
+            // 
+            // player
+            // 
+            this.player.Image = global::ZSG.Properties.Resources.up;
+            this.player.Location = new System.Drawing.Point(413, 355);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(71, 100);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(922, 653);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.health);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtAmmo);
+            this.Name = "Form1";
+            this.Text = "ZombieShooterGame";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label txtAmmo;
+        private Label txtScore;
+        private Label health;
+        private ProgressBar healthBar;
+        private PictureBox player;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
